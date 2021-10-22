14a0e68ca86cfa7a901abbc003622cffc0b556b4
TheRoniOne <ronidani99@hotmail.com>
2021-10-21 19:34:46 -0500

    improving fish conf

--- config.fish
+++ config.fish
@@ -2,4 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set fish_greeting
+
+# ENV vars
+set EDITOR "nvim"
+set TERM "kitty"
+
+# Aliases
+alias icat "kitty +kitten icat"
+
 starship init fish | source
