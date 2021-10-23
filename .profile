b8e2d615b19fd3b52ca0a52f64bb493968ef1097
TheRoniOne <ronidani99@hotmail.com>
2021-10-23 10:26:10 -0500

    updated fish

--- .profile
+++
@@ -1,4 +0,0 @@
-PATH=$PATH:/home/roni/soft/julia-1.6.2/bin:/home/roni/soft/jdk-11.0.11+9/bin
-eval $(keychain --eval --quiet /home/roni/.ssh/github-arcovm)
-EDITOR=/usr/bin/nvim
-
--- fish/config.fish
+++ fish/config.fish
@@ -2,15 +2,19 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set fish_greeting
-
 fish_add_path /home/roni/soft/julia-1.6.2/bin /home/roni/soft/jdk-11.0.11+9/bin
 
 # ENV vars
 set EDITOR "nvim"
 set TERM "kitty"
+set -x MANPAGER "nvim -c 'set ft=man' -"
 
 # Aliases
 alias icat "kitty +kitten icat"
+alias ls="exa -al --color=always --group-directories-first"
+alias ls -la "exa -a --color=always --group-directories-first"
+
+# Evals
+eval (keychain --eval --agents ssh -Q --quiet $HOME/.ssh/github-arcovm)
 
 starship init fish | source
