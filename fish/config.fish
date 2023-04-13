3ce9fce900c85da706c6b5d09631dc9f274ff929
TheRoniOne <ronidani99@hotmail.com>
2023-04-13 10:10:09 -0500

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -4,11 +4,11 @@ end
 
 # ENV vars
 set EDITOR "nvim"
-set TERM "kitty"
+# set TERM "alacritty"
 set -x MANPAGER "nvim -c 'set ft=man' -"
 
 # Aliases
 
 # Evals
-keychain --eval $HOME/.ssh/id_ed25519 | source
+keychain --eval --quiet $HOME/.ssh/id_ed25519 | source
 starship init fish | source
