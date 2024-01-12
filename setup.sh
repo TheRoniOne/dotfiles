39737301ac2da31782305c69251df20bf8a0088c
TheRoniOne <ronidani99@hotmail.com>
2024-01-12 01:28:20 -0500

    Add zsh config

--- setup.sh
+++ setup.sh
@@ -2,4 +2,6 @@
 
 rm -f $HOME/.config/fish/config.fish && ln -sr ./fish/config.fish $HOME/.config/fish/config.fish
 
+rm -f $HOME/.zshrc && ln -sr ./.zshrc $HOME/.zshrc
+
 rm -f $HOME/.config/starship.toml && ln -sr ./starship.toml $HOME/.config/starship.toml
