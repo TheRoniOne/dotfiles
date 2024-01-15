35d5e332fa122fc12e8bc285d3309422b4a8c53e
TheRoniOne <ronidani99@hotmail.com>
2024-01-15 11:05:13 -0500

    Remove old files

--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ rm -f $HOME/.config/fish/config.fish && ln -sr ./fish/config.fish $HOME/.config/
 rm -f $HOME/.zshrc && ln -sr ./.zshrc $HOME/.zshrc
 
 rm -f $HOME/.config/starship.toml && ln -sr ./starship.toml $HOME/.config/starship.toml
+
+rm -f $HOME/.alacritty.toml && ln -sr ./alacritty.toml $HOME/.alacritty.toml
