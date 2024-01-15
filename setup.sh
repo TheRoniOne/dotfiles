98eec6ecc796cd81a922569e6c30cfd2e55531a1
TheRoniOne <ronidani99@hotmail.com>
2024-01-15 17:26:44 -0500

    Add zellij conf

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-rm -f $HOME/.config/fish/config.fish && ln -sr ./fish/config.fish $HOME/.config/fish/config.fish
-
 rm -f $HOME/.zshrc && ln -sr ./.zshrc $HOME/.zshrc
 
 rm -f $HOME/.config/starship.toml && ln -sr ./starship.toml $HOME/.config/starship.toml
 
 rm -f $HOME/.alacritty.toml && ln -sr ./alacritty.toml $HOME/.alacritty.toml
+
+mkdir ~/.config/zellij
+rm -f $HOME/.config/zellij/config.kdl && ln -sr ./zellij-config.kdl $HOME/.config/zellij/config.kdl
